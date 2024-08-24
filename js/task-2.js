21a0f993f46f66188fd422d8404262d47116f4d1
Oleksandr Lisovyk <160383366+Lisovyk212@users.noreply.github.com>
2024-08-24 12:42:45 +0300

    commit message

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,6 @@
+'use strict';
 function getShippingMessage(country, price, deliveryFee) {
-    totalPrice = price + deliveryFee;
+    const totalPrice = price + deliveryFee;
     return `Shipping to ${country} will cost ${totalPrice} credits`;
 };
 console.log(getShippingMessage("Australia", 120, 50));
